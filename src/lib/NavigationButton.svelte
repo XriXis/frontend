31cee04709aa2a00fad906c48e83c6b70953645b
XriXis <xzsaythjpf@gmail.com>
2024-07-04 02:44:23 +0300

    Work on gh-pages

--- src/lib/NavigationButton.svelte
+++ src/lib/NavigationButton.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    export let ref: string;
+    export let href: string;
     export let label: string;
 
     let currentUrl = new URL(window.location.href);
-    let refUrl = new URL(ref, window.location.origin);
+    let refUrl = new URL(href, window.location.origin);
     let btnClr = currentUrl.pathname === refUrl.pathname ? "#4978ab" : "#2f5d88";
 </script>
 
-<button on:click={() => { window.location.href = ref; }} style="background-color: {btnClr};">{label}</button>
+<button on:click={() => { window.location.href = href; }} style="background-color: {btnClr};">{label}</button>
 
 <style>
     button {
